150174cec8a24301063c369f773c31f79d365c3b
Blanc Gaspard <blanc.g@mail.fr>
2022-06-23 13:48:33 +0200

    Ajout du fichier script.js

---
+++ js/script.js
@@ -0,0 +1,38 @@
+
+
+/*requête de l’API pour lui demander l’ensemble des produits*/
+
+fetch('http://localhost:3000/api/products')
+  .then(function(reponse) {
+    if (reponse.ok) {
+      return reponse.json();
+    }
+  })
+  .then(function(donnees) {
+    console.log(donnees);
+    produit(donnees);
+  })
+  .catch(function(erreur) {
+    console.log(erreur);
+  });
+
+const produits = document.getElementById("items");
+
+  
+let produit = function(donnees) {
+  for (let i = 0; i < donnees.length; i++) {
+    const codeHtmlProduits =
+    `<a href="product.html?id=${donnees[i]._id}">
+        <article>
+          <img src="${donnees[i].imageUrl}" alt="${donnees[i].atlTxt}" />
+          <h3 class="productName">${donnees[i].name}</h3>
+          <p class="productDescription">${donnees[i].description}</p>
+        </article>
+      </a>`;
+    produits.insertAdjacentHTML("afterbegin", codeHtmlProduits);
+  }
+};
+
+/*let tailleCarte = document.querySelector('.items a');
+
+tailleCarte.style.maxWidth='21px';*/
